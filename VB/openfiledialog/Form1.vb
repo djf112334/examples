bc615a5ae66be78c144e95b48b636b6d3e0951d6
lockheedsr71 <mbksoft@gmail.com>
2016-08-15 12:40:05 +0430

    select path by file dialog

---
+++ VB/openfiledialog/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox2.Text = OpenFileDialog1.FileName
+
+
+        End If
+
+
+    End Sub
+End Class
+
+
