04bae1b1bdae21978cc8e7ecea5dcd94ffc01ea3
lockheedsr71 <mbksoft@gmail.com>
2016-10-25 17:07:58 +0330

    xml readwrite

---
+++ vb.net/XML_ReadWrite/XML_ReadWrite/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label4.Location = New System.Drawing.Point(139, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(271, 24)
+        Me.Label4.TabIndex = 23
+        Me.Label4.Text = "Write and Read XML from FILE"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(335, 172)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 22
+        Me.Button2.Text = "Read"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(129, 171)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "Write"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(331, 113)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 20
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(331, 87)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 19
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(331, 61)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 18
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(111, 110)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 17
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(111, 84)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 16
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(111, 58)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 15
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(50, 122)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Label3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Label2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(493, 240)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ vb.net/XML_ReadWrite/XML_ReadWrite/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports System.Xml
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        REM ===========================WRITE ==========================
+
+        'first let's check if there is a file MyXML.xml into our application folder
+        'if there wasn't a file something like that, then let's create a new one.
+
+        Dim curdir, xmldir As String
+        curdir = My.Application.Info.DirectoryPath
+        xmldir = curdir & "\MyXML.xml"
+
+
+        'declare our xmlwritersettings object
+        Dim settings As New XmlWriterSettings()
+
+        'lets tell to our xmlwritersettings that it must use indention for our xml
+        settings.Indent = True
+
+        'lets create the MyXML.xml document, the first parameter was the Path/filename of xml file
+        ' the second parameter was our xml settings
+        Dim XmlWrt As XmlWriter = XmlWriter.Create(curdir & "\MyXML.xml", settings)
+
+        With XmlWrt
+
+            ' Write the Xml declaration.
+            .WriteStartDocument()
+
+            ' Write a comment.
+            .WriteComment("XML settings.")
+
+            ' Write the root element.
+            .WriteStartElement("configs")
+
+            ' Start our first person.
+            .WriteStartElement("tab_1")
+
+            ' The person nodes.
+
+            .WriteStartElement("FirstName")
+            REM .WriteString("akbar")
+            .WriteString(TextBox1.Text)
+            .WriteEndElement()
+
+            .WriteStartElement("LastName")
+            .WriteString(TextBox2.Text)
+            .WriteEndElement()
+
+            .WriteStartElement("tel")
+            .WriteString(TextBox3.Text)
+            .WriteEndElement()
+
+            ' The end of this person.
+            .WriteEndElement()
+
+            ' Close the XmlTextWriter.
+            .WriteEndDocument()
+            .Close()
+
+        End With
+
+        MessageBox.Show("XML file saved.")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'check if file myxml.xml is existing
+        Dim curdir, xmldir As String
+        curdir = My.Application.Info.DirectoryPath
+        xmldir = curdir & "\MyXML.xml"
+
+        If (IO.File.Exists(xmldir)) Then
+
+            'create a new xmltextreader object
+            'this is the object that we will loop and will be used to read the xml file
+            Dim document As XmlReader = New XmlTextReader(curdir & "\MyXML.xml")
+
+            'loop through the xml file
+            While (document.Read())
+
+                Dim type = document.NodeType
+
+                'if node type was element
+                If (type = XmlNodeType.Element) Then
+
+                    'if the loop found a <FirstName> tag
+                    If (document.Name = "FirstName") Then
+
+                        TextBox4.Text = document.ReadInnerXml.ToString()
+
+                    End If
+
+                    'if the loop found a <LastName tag
+                    If (document.Name = "LastName") Then
+
+                        TextBox5.Text = document.ReadInnerXml.ToString()
+
+                    End If
+                    If (document.Name = "tel") Then
+
+                        TextBox6.Text = document.ReadInnerXml.ToString()
+
+                    End If
+
+                End If
+
+            End While
+            document.Close()
+
+        Else
+
+            MessageBox.Show("The filename you selected was not found.")
+        End If
+    End Sub
+End Class
