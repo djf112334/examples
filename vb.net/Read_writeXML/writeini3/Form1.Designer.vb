53f42fe21ffcf6870859c29c7d6f1b54816a6c83
lockheedsr71 <mbksoft@gmail.com>
2016-10-25 10:48:15 +0330

    write and read xml into file needed for some progs setting

---
+++ vb.net/Read_writeXML/writeini3/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(22, 45)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "WR xml"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(314, 48)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(180, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(314, 22)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(180, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(365, 100)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "read"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(314, 74)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(180, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(540, 167)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ vb.net/Read_writeXML/writeini3/Form1.vb
@@ -0,0 +1,118 @@
+﻿Imports System.Security.Cryptography.X509Certificates
+Imports System.Xml
+Imports System.IO
+Imports System.Text
+
+
+
+
+Public Class Form1
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'first let's check if there is a file MyXML.xml into our application folder
+        'if there wasn't a file something like that, then let's create a new one.
+
+        If IO.File.Exists("MyXML.xml") = False Then
+
+            'declare our xmlwritersettings object
+            Dim settings As New XmlWriterSettings()
+
+            'lets tell to our xmlwritersettings that it must use indention for our xml
+            settings.Indent = True
+
+            'lets create the MyXML.xml document, the first parameter was the Path/filename of xml file
+            ' the second parameter was our xml settings
+            Dim XmlWrt As XmlWriter = XmlWriter.Create("o:\MyXML.xml", settings)
+
+            With XmlWrt
+
+                ' Write the Xml declaration.
+                .WriteStartDocument()
+
+                ' Write a comment.
+                .WriteComment("XML settings.")
+
+                ' Write the root element.
+                .WriteStartElement("configs")
+
+                ' Start our first person.
+                .WriteStartElement("tab_1")
+
+                ' The person nodes.
+
+                .WriteStartElement("FirstName")
+                .WriteString("akbar")
+                .WriteEndElement()
+
+                .WriteStartElement("LastName")
+                .WriteString("alizadeh")
+                .WriteEndElement()
+
+                .WriteStartElement("tel")
+                .WriteString("23589963")
+                .WriteEndElement()
+
+
+                ' The end of this person.
+                .WriteEndElement()
+
+                ' Close the XmlTextWriter.
+                .WriteEndDocument()
+                .Close()
+
+            End With
+
+            MessageBox.Show("XML file saved.")
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        'check if file myxml.xml is existing
+        If (IO.File.Exists("o:\MyXML.xml")) Then
+
+            'create a new xmltextreader object
+            'this is the object that we will loop and will be used to read the xml file
+            Dim document As XmlReader = New XmlTextReader("o:\MyXML.xml")
+
+            'loop through the xml file
+            While (document.Read())
+
+                Dim type = document.NodeType
+
+                'if node type was element
+                If (type = XmlNodeType.Element) Then
+
+                    'if the loop found a <FirstName> tag
+                    If (document.Name = "FirstName") Then
+
+                        TextBox1.Text = document.ReadInnerXml.ToString()
+
+                    End If
+
+                    'if the loop found a <LastName tag
+                    If (document.Name = "LastName") Then
+
+                        TextBox2.Text = document.ReadInnerXml.ToString()
+
+                    End If
+                    If (document.Name = "tel") Then
+
+                        TextBox3.Text = document.ReadInnerXml.ToString()
+
+                    End If
+
+                End If
+
+            End While
+
+        Else
+
+            MessageBox.Show("The filename you selected was not found.")
+        End If
+
+    End Sub
+End Class
+
+
