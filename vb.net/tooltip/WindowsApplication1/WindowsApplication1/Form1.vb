fe356945e29a5258f6ec17523007a287a35a4cf7
lockheedsr71 <mbksoft@gmail.com>
2016-11-28 20:16:09 +0330

    simple and good tooltip popup menu

---
+++ vb.net/tooltip/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+                           Handles MyBase.Load
+
+        NotifyIcon1.BalloonTipText = "Tool Tip Text for Windows Application"
+        NotifyIcon1.Text = "Windows Application"
+        NotifyIcon1.ShowBalloonTip(5000)
+        CreateContextMenu()
+
+    End Sub
+
+    Public Sub CreateContextMenu()
+
+        'Define New Context Menu and Menu Item 
+        Dim contextMenu As New ContextMenu
+        Dim menuItem As New MenuItem("Exit")
+        contextMenu.MenuItems.Add(menuItem)
+
+        ' Associate context menu with Notify Icon 
+        NotifyIcon1.ContextMenu = contextMenu
+
+        'Add functionality for menu Item click 
+        AddHandler menuItem.Click, AddressOf menuItem_Click
+
+    End Sub
+
+    Private Sub menuItem_Click(ByVal sender As Object, ByVal e As System.EventArgs)
+        Me.Close()
+    End Sub
+End Class
